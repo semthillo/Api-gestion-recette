27841f1f64573bc4c9799cc0eeecddc5b475eccf
Ousmanly <lyousmane492@gmail.com>
2024-09-14 23:00:42 +0200

    Update post crud

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const recipeRoutes = require('./routes/recipeRoutes')
+import express from 'express'
+import bodyParser from 'body-parser'
+import recipeRoutes from './routes/recipeRoutes.js'
 const app = express()
 
 app.use(bodyParser.json())
 
 app.use('/api', recipeRoutes);
+
 const port = 3005
 app.listen(port, () => {
     console.log(`Server in runing in port ${port}`);

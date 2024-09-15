aa75174e59dbb1c2f50d468b5e34d4720dc1a050
mariem2012 <mariemdianibafa00@gmail.com>
2024-09-15 14:04:09 +0000

    Resolve Conflit to recipeController file

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,34 +1,37 @@
-import db from '../config/db.js';
-import { validationResult } from 'express-validator'; 
+import db from "../config/db.js";
+import { validationResult } from "express-validator";
 
 export const createRecipe = async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
-    const { title, ingredient, type } = req.body; 
+  const { title, ingredient, type } = req.body;
 
-    try {
-        const [results] = await db.query('INSERT INTO recipes (title, ingredient, type) VALUES (?, ?, ?)', [title, ingredient, type]);
-        res.status(200).json({ id: results.insertId });
-    } catch (err) {
-        console.error('Error inserting recipe:', err);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+  try {
+    const [results] = await db.query(
+      "INSERT INTO recipes (title, ingredient, type) VALUES (?, ?, ?)",
+      [title, ingredient, type]
+    );
+    res.status(200).json({ id: results.insertId });
+  } catch (err) {
+    console.error("Error inserting recipe:", err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 export const getAllRecipes = async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
-    try {
-        const [results] = await db.query('SELECT * FROM recipes')
-        console.table(results );
-        res.status(200).json(results);
-    } catch (err) {
-        console.error('Error listing recipe:', err);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+  try {
+    const [results] = await db.query("SELECT * FROM recipes");
+    console.table(results);
+    res.status(200).json(results);
+  } catch (err) {
+    console.error("Error listing recipe:", err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
